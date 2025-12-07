4617998556fa33f95b188dde404a5fceab8fa831
hngbfv3399 <hngbfv3399@gmail.com>
2025-12-07 23:14:35 +0900

    test: 세팅 테스트

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,9 @@
-// unused variable
-const test = 123
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-// React import 필요 없지만 규칙 상 react-hooks 오류 유발
-import { useEffect } from 'react'
-
-export default function TestError() {
-
-
-
-  const [count, setCount] = React.useState(0)  // ❌ React 19에서는 자동 import, ESLint가 React undefined라 에러낼 가능성 높음
-
-  useEffect(() => {
-    console.log("test")
-  }
-    , []) // ❌ Prettier + ESLint 포맷 에러 유도
-
-  return <div>
-    <p> count: {count}</p>
-  </div>
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
